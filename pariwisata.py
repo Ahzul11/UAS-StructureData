fde7d9c10b51d3fad8261e1b82214cab25c47082
Ahzul11 <76717485+Ahzul11@users.noreply.github.com>
2021-07-13 06:31:18 +0000

    done

--- pariwisata.py
+++ pariwisata.py
@@ -81,7 +81,6 @@ class tampilan:
         # windows
         if name == 'nt':
             _ = system('cls')
-
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
